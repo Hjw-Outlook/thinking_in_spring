cb485c85758fbb1fa5165fdcdc02908986dfe3ec
tedu <tedu@DESKTOP-67KFJT0>
2019-05-06 09:47:19 +0800

    new

--- abc/src/main/java/abc/A.java
+++ abc/src/main/java/abc/A.java
@@ -4,9 +4,6 @@ public class A {
 
 	public static void main(String[] args) {
 		System.out.println("hello world!");
-		System.out.println(123);
-		System.out.println(456);
-		System.out.println(123);
-		System.out.println(1);
+		System.out.println(1231);
 	}
 }
