d1bc57b9c8a67f4e13216d8b2f29b88d7f516ec9
tedu <tedu@DESKTOP-67KFJT0>
2019-05-06 09:29:54 +0800

    new

--- abc/src/main/java/abc/A.java
+++ abc/src/main/java/abc/A.java
@@ -3,6 +3,10 @@ package abc;
 public class A {
 
 	public static void main(String[] args) {
-		
+		System.out.println("hello world!");
+		System.out.println(123);
+		System.out.println(456);
+		System.out.println(123);
+		System.out.println(1);
 	}
 }
